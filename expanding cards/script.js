e63677e47c71b02068832f7a4f6a3fe2fb274239
Eusebiu-Georgian David <80643905+seby6191@users.noreply.github.com>
2022-10-07 13:03:08 +0300

    Add files via upload

---
+++ expanding cards/script.js
@@ -0,0 +1,15 @@
+const panels = document.querySelectorAll('.panel');
+panels.forEach((panel)=>{
+   // console.log(panel);
+    panel.addEventListener('click',()=>{
+    //console.log(123);
+    removeActiveClasses()
+    panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses(){
+    panels.forEach(panel=>{
+        panel.classList.remove('active')
+    })
+}
